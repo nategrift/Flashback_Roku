13e4c6336ad0f7ace5f1747d06360b8c58ff20f0
nategrift <nategrift@gmail.com>
2021-03-24 13:04:08 -0400

    Added update .sh file

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#install 
+npm install --only=prod
+ 
+# Go to client folder
+cd client
+
+cp ../../config.example.js src/config.js
+
+# Create dist folder
+npm install
+npm run build
+
+cp -r ../../media dist/media
+
+#return to original  folder
+cd ..
+
+pm2 restart flashback
+
+
+
